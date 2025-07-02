3f7ed53c1fc19b0ffcaed411d9ede95e02bc4487
Ivan Matviichuk <138168505+imatvii@users.noreply.github.com>
2025-07-01 19:13:52 -0700

    boom

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pornhub.com"
           target="_blank"
           rel="noopener noreferrer"
         >
